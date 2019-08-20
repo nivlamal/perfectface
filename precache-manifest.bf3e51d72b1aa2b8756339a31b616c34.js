0e614628da994427a221e9f64a51c921999cf43e
nivlamal <malnivla@gmail.com>
2019-08-20 15:41:59 +0800

    Updates

---
+++ precache-manifest.bf3e51d72b1aa2b8756339a31b616c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466ae98d469f9bfb9993b8e423627064",
+    "url": "/perfectface/index.html"
+  },
+  {
+    "revision": "23a553c51fd5d45bbd57",
+    "url": "/perfectface/static/css/main.6db874d3.chunk.css"
+  },
+  {
+    "revision": "c22fe1ce3a8b5af63a41",
+    "url": "/perfectface/static/js/2.68c0fc53.chunk.js"
+  },
+  {
+    "revision": "23a553c51fd5d45bbd57",
+    "url": "/perfectface/static/js/main.9425c9dd.chunk.js"
+  },
+  {
+    "revision": "5f11ac6c4cb7fb850296",
+    "url": "/perfectface/static/js/runtime~main.f515fd0a.js"
+  }
+]);
